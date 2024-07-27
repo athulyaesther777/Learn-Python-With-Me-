3e1f83e1c13e1af50cd0e90cd82fe9257bf806a1
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-27 15:28:22 +0530

    Add files via upload

---
+++ part4.py
@@ -0,0 +1,26 @@
+
+                #Collecting The User Inputs
+
+input("Can I Know More About You:")
+ #output: Can I Know More About You:(user can say whatever they want )
+ #this is a example -
+ #output: Can I Know More About You:Yes , I am a Ai Engineer.
+
+
+ #now we are going to turn it into variable
+ #the otput is just an example
+ 
+name = input("Your name Please: ")  # Output: Your name Please: Athulya Esther
+work = input("What do you do: ")    # Output: What do you do: AI Engineer
+print("Hello! " + name + " ,you are a " + work + " then")  # Output: Hello! Athulya Esther, you are a AI Engineer then
+place = input("Where do you work?: ")  # Output: Where do you work?: Ireland
+
+
+#output
+
+""""
+Your name Please: Athulya Esther
+What do you do: AI Engineer
+Hello! Athulya Esther, you are an AI Engineer then.
+Where do you work?: Ireland
+"""
