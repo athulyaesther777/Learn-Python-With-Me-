d5eea518ecb627202b9fd4ce35493f3401694e4c
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-28 21:32:27 +0530

    Add files via upload

---
+++ part6madlibgame.py
@@ -0,0 +1,30 @@
+
+                 # Mad Libs Game
+
+name = input("Enter a name:")
+object = input("Enter a object:")
+emotion = input("Enter a emotion:")
+
+print("Yesterday, " + name + " came to my place,")
+print("to give me a piece of " + object + " ")
+print("And I " + emotion + " her.")
+
+#output
+"""
+eg:1
+Enter a name:ESther
+Enter a object:cake
+Enter a emotion:loved
+Yesterday, ESther came to my place,
+to give me a piece of cake 
+And I loved her.
+
+eg:2
+Enter a name:annie
+Enter a object:chocolate
+Enter a emotion:avoided 
+Yesterday, annie came to my place,
+to give me a piece of chocolate 
+And I avoided  her.
+
+"""
