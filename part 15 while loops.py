9b6bac9968fbeab9c14d073284f21a5957e05aff
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-29 22:22:35 +0530

    Add files via upload

---
+++ part 15 while loops.py
@@ -0,0 +1,61 @@
+           # Example 1: Basic While Loop
+a = 1
+while a <= 15:
+    print(a)  # Prints the current value of 'a'
+    a += 1   # Increments 'a' by 1 after each iteration
+
+print("Done with the looping")
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# Done with the looping
+
+# Example 2: Loop with Condition
+a = 1
+while a < 15:
+    print(a)  # Prints the current value of 'a'
+    a += 1   # Increments 'a' by 1 after each iteration
+
+print("Done with the looping")
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# Done with the looping
+
+# Example 3: Loop with Increment
+a = 2
+while a < 15:
+    print(a)  # Prints the current value of 'a'
+    a += 5   # Increments 'a' by 5 after each iteration
+
+print("Done with the looping")
+# Output:
+# 2
+# 7
+# 12
+# Done with the looping
