f9ebd8fd698bccfe33bbc69f044d98c6e3e47c98
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-29 12:10:42 +0530

    Add files via upload

---
+++ part9tuples.py
@@ -0,0 +1,28 @@
+                  # Creating a tuple
+                  
+Numbers = (7, 5)
+print(Numbers)  # Output: (7, 5)
+
+# Accessing an element in a tuple
+print(Numbers[0])  # Output: 7
+
+# Attempting to modify a tuple will raise an error
+"""
+Numbers[0] = 12
+print(Numbers)
+"""
+# This will raise TypeError: 'tuple' object does not support item assignment
+
+# Creating a tuple
+Numbers = (7, 5)
+print(Numbers)         # Output: (7, 5)
+
+# Accessing an element in a tuple
+print(Numbers[0])      # Output: 7
+
+# Attempting to modify a tuple will raise an error
+"""
+Numbers[0] = 12
+print(Numbers)
+"""
+# This will raise TypeError: 'tuple' object does not support item assignment
