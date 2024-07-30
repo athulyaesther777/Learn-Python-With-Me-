55a7d9c02d48793b41890ba56ffea2c7fbc81224
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-30 16:03:25 +0530

    Add files via upload

---
+++ part 20 madlib project.py
@@ -0,0 +1,25 @@
+             # Creating a MadLib Game
+
+# Collecting user inputs for the MadLib
+name = input("Enter the name: ")
+place = input("Enter the name of Place: ")
+relation = input("Enter the relation: ")
+
+# Generating the MadLib story
+print("Once upon a time in " + place + ".")
+print("I met a person, and now he is my " + relation + ".")
+print("His name was " + name + ".")
+print("And now I got graduated from the " + place + ".")
+
+"""
+Example output:
+
+Enter the name: Ajil
+Enter the name of Place: College
+Enter the relation: Brother
+
+Once upon a time in College.
+I met a person, and now he is my Brother.
+His name was Ajil.
+And now I got graduated from the College.
+"""
