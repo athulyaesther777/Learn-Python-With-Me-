02ccffa5803111bbd3ff5abc7f190e0134581bdd
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-07-30 15:50:06 +0530

    Add files via upload

---
+++ part 19  project 1.py
@@ -0,0 +1,20 @@
+        #Collect baisc informatoin from the user
+
+# Collecting the first name from the user
+name1 = input("What is your name: ")
+print("My name is " + name1 + ", what is your name?")
+
+# Collecting the second name from the user
+name2 = input("Enter the second name: ")
+print("My name is " + name2 + ". Thanks for asking")
+
+"""
+Example output:
+
+What is your name: Alia
+My name is Alia, what is your name?
+Enter the second name: Randbir
+My name is Randbir. Thanks for asking
+"""
+
+
