358828796ac2122c3a3ef7774885a78dc28bc3da
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-15 19:06:35 +0530

    Create wwp part 2.py

---
+++ wwp part 2.py
@@ -0,0 +1,18 @@
+print(" Welcome to the band name generator ")
+# Output: 
+# Welcome to the band name generator
+
+colour = input("Which Is Your Favorite Colour?\n")
+# Prompts the user to enter their favorite color.
+# Example input: 
+# Blue
+
+name = input("What is your name?\n")
+# Prompts the user to enter their name.
+# Example input: 
+# Alice
+
+print(" Your band name is : " + colour + " " + name)
+# Combines the color and name to generate and display a band name.
+# Example output: 
+# Your band name is : Blue Alice
