34fa39f25b2c0b18897fd06a849a6ddf20140046
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-20 17:44:27 +0530

    Create maths_operations.py

---
+++ maths_operations.py
@@ -0,0 +1,49 @@
+print(4 + 6)       # Addition
+# Output: 10
+
+print(6 / 2)       # Division
+# Output: 3.0
+
+print(8 * 9)       # Multiplication
+# Output: 72
+
+print(4 - 0)       # Subtraction
+# Output: 4
+
+print(10 // 3)     # Integer Division
+# Output: 3 (no decimal part)
+
+print(37 // 5)     # Integer Division
+# Output: 7 (no decimal part)
+
+print(7 // 8)      # Integer Division
+# Output: 0 (no decimal part)
+
+print(2 ** 2)      # Exponentiation
+# Output: 4
+
+print(10 ** 0)     # Exponentiation
+# Output: 1
+
+print(7 ** 3)      # Exponentiation
+# Output: 343
+
+print(3 + 6 * 7 / 8)   # Multiplication and Division first
+# 6 * 7 = 42
+# 42 / 8 = 5.25
+# 3 + 5.25 = 8.25
+# Output: 8.25
+
+print(4 * 4 + 4 / 4 - 4)  # Multiplication and Division first
+# 4 * 4 = 16
+# 4 / 4 = 1
+# 16 + 1 = 17
+# 17 - 4 = 13
+# Output: 13
+
+print(4 * (4 + 4) / 4 - 4)  # Brackets first
+# Inside brackets: 4 + 4 = 8
+# 4 * 8 = 32
+# 32 / 4 = 8
+# 8 - 4 = 4
+# Output: 4
