a957ee47165b6340ebbde3fc7783260cf34d68b3
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-28 23:10:51 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,16 @@
+# Firstly, use the 'def' keyword to define a new function
+def my_function():
+    # Inside the function, do this: print a message
+    print("Athu.....")
+
+# Then, call the function using its name followed by parentheses
+my_function()
+
+# Finally, print another message outside of the function
+print("I am waiting for you.")
+"""
+1. Firstly, use the 'def' keyword to define a function.
+2. Inside the function, do this: define the actions you want the function to perform.
+3. Finally, call the function by using its name followed by parentheses.
+The function will execute the code inside its block.
+"""
